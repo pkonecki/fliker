0b2728d16539db6edacbe4618fb450a2e396c418
meneo.tk <meneo.tk@gmail.com>
2011-04-07 09:56:51 +0000

    inscription changé

--- inscription/index.php
+++ inscription/index.php
@@ -7,21 +7,46 @@
 <h1>Inscription</h1> 
 <?php
 	include("../includes/paths.php");
-	include($webservices."/normalTask_getChampsAdherents.php");
-	$tab = getChampsAdherents();
-	print "<h2>Exemple Inscription</h2>";
-	print "<FORM>";
-	foreach($tab as $row){
-		if($row[inscription]==1){
-		if($row[type]==="varchar")
-		print "".$row[description]." : <INPUT type=text name=".$row[nom]."><br/>";
-		
-		if($row[type]==="tinyint")
-		print "".$row[description]." : <INPUT type=checkbox name=".$row[nom]."><br/>";
+	include("normalTask_getChampsAdherents.php");
+	if ($_POST['action'] == 'submitted') {
+$tab = getChampsAdherents();
+		print "<h2>Bla</h2>";
+		print "<FORM action=\"index.php\" method=\"POST\">";
+		foreach($tab as $row){
+			if($row[inscription]==1){
+			if($row[type]==="varchar")
+			print "<LABEL for =".$row[nom]." >".$row[description]."</LABEL> : <INPUT type=text name=".$row[nom]."  value=".$_POST[$row[nom]]."><br/>";
+			
+			if($row[type]==="tinyint"){
+				print "<LABEL for =".$row[nom]." >".$row[description]."</LABEL> : <INPUT type=checkbox name=".$row[nom]." value=".$_POST[$row[nom]]."><br/>";
+				print $_POST[$row[nom]];
+			}
+			}
 		}
+		print "<input type=\"hidden\" name=\"action\" value=\"submitted\" />";
+		print "<INPUT type=\"submit\" value=\"Send\">";
+		print "</FORM>";
+
+	} else {
+	
+		$tab = getChampsAdherents();
+		print "<h2>Exemple Inscription</h2>";
+		print "<FORM action=\"index.php\" method=\"POST\">";
+		foreach($tab as $row){
+			if($row[inscription]==1){
+			if($row[type]==="varchar")
+			print "<LABEL for =".$row[nom]." >".$row[description]."</LABEL> : <INPUT type=text name=".$row[nom]."><br/>";
+			
+			if($row[type]==="tinyint")
+			print "<LABEL for =".$row[nom]." >".$row[description]."</LABEL> : <INPUT type=checkbox name=".$row[nom]."><br/>";
+			}
+		}
+		print "<input type=\"hidden\" name=\"action\" value=\"submitted\" />";
+		print "<INPUT type=\"submit\" value=\"Send\">";
+		print "</FORM>";
 	}
-	print "</FORM>";
-	print "<h2>Exemple modification</h2>";
+	
+	/*print "<h2>Exemple modification</h2>";
 	print "<FORM>";
 	foreach($tab as $row){
 		if($row[user_editable]==1){
@@ -32,7 +57,8 @@
 		print "".$row[description]." : <INPUT type=checkbox name=".$row[nom]."><br/>";
 		}
 	}
-	print "</FORM>";	
+	print "</FORM>";
+	*/	
 
 ?>
 </body></html> 
