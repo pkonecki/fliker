b2cd8a58183a0203a65d02d719914971eda65e5b
meneo.tk <meneo.tk@a3323499-10a8-7d40-5257-ad8efff166ca>
2011-06-17 06:19:09 +0000

    get associations

--- includes/fiche_asso.php
+++ includes/fiche_asso.php
@@ -9,13 +9,14 @@ if ($_POST['action'] == 'modification') {
 		
 		print '<tr><td class="label"><LABEL for ="nom" >Nom</LABEL> : </td><td><INPUT type=text name="nom" id="nom" value="'.$_POST['nom'].'"></td></tr>';
 		print '<tr><td class="label"><LABEL for ="description" >Nom</LABEL> : </td><td><TEXTAREA type=text name="description" id="description" value="'.$_POST['description'].'"></td></tr>';
-		
+		print '<tr><td class="label"><LABEL for ="logo_asso" >Logo</LABEL> : </td><td><INPUT type=file name=logo_asso ></td></tr>';
+		print '<tr><td class="label"><LABEL for ="url" >URL</LABEL> : </td><td><INPUT type=text name="url" id="url" value="'.$_POST['url'].'"></td></tr>';
 		print '<input type=\'hidden\' name=\'action\' value=\'submitted\' />';
 		print '<tr><td colspan="2"><INPUT type=\'submit\' value=\'Send\'></td></tr>';
 		print '</table>';
 		print '</FORM>';
 			
-}
+} else
 if ($_POST['action'] == 'new') {
 		
 			
@@ -27,7 +28,23 @@ else {
 		
 	}
 	if(!(strcmp($_SESSION['user'],"") == 0)){
-	
+		include_once("getAssociations.php");
+		$tab=getAssociations($_SESSION['uid']);
+		if(empty($_GET['asso'])){
+			print '<h2>Vos associations</h2>';	
+			foreach($tab as $asso){
+				print '<a href=index.php?page=3&asso='.$asso['id'].'>'.$asso['nom'].'</a>  ';
+				
+			}
+			
+			
+		} else {
+			print '<table>';
+			print '<tr><td class="label">Nom : </td><td>'.$tab[$_GET['asso']]['nom'].'</td></tr>';
+			print '<tr><td class="label">Description : </td><td>'.$tab[$_GET['asso']]['description'].'</td></tr>';
+			print '<tr><td class="label">Url : </td><td>'.$tab[$_GET['asso']]['url'].'</td></tr>';		
+			print '</table>';
+		}
 	
 	}
 	else {
--- menu.php
+++ menu.php
@@ -2,5 +2,6 @@
 print '<ul id="main_menu">
 		<li><a href="index.php?page=1">Fiche Adhérent</a></li>
 		<li><a href="index.php?page=2">Rechercher</a></li>
+		<li><a href="index.php?page=3">Fiche Asso</a></li>
 		</ul>';
 ?>
