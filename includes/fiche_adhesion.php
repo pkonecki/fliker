8ed616b1212437daa2920bab9961ff0746bbf95e
meneo.tk <meneo.tk@a3323499-10a8-7d40-5257-ad8efff166ca>
2011-08-09 22:42:23 +0000

    Paiements ok sauf delete

--- includes/fiche_adhesion.php
+++ includes/fiche_adhesion.php
@@ -79,11 +79,7 @@ if ($_POST['action'] == 'select_assos' && $self && !empty($_POST['cre']) ) {
 	print "<span hidden id=id_statut_adh>$id_statut_adh</span>";
 	print '</TABLE>
 	<INPUT type="submit" value="Valider"><INPUT type="reset" class="reset" value="Remettre à zéro" ></FORM>';
-} if ($_POST['action']==='nouveau_paiement') {
-	print_r_html($_POST);
-
 }
-
  else{
 	if ($_POST['action'] == 'submitted' && $self){
 
@@ -92,6 +88,15 @@ if ($_POST['action'] == 'select_assos' && $self && !empty($_POST['cre']) ) {
 	}
 	if ($_POST['action'] === 'suppression_ads'){
 		delAdhesion($_POST['id_ads']);
+	}
+	if ($_POST['action']==='nouveau_paiement') {
+		//print_r_html($_POST);
+		if(empty($_POST['sup']) || empty($_POST['type']) || empty($_POST['promo']) || empty($_POST['num']) ){
+			print "<pre>Il y a une erreur dans le paiement</pre>";
+		} else {
+			addPaiement($_POST);
+		}
+
 	}
 	if(!(strcmp($_SESSION['user'],"") == 0)){
 		print '<ul id="submenu"><li><a href="index.php?page=1&adh='.$id_adh.'">Fiche Adhérent</a></li><li><a class="selected" href="index.php?page=7&adh='.$id_adh.'">Adhésions</a></li></ul>';
@@ -130,7 +135,7 @@ if ($_POST['action'] == 'select_assos' && $self && !empty($_POST['cre']) ) {
 
 		//Facture
 		print '<h2>Facture</h2>
-				<FORM action="index.php?page=7" method="POST">
+				<FORM action="index.php?page=7&adh='.$id_adh.'&asso='.$current_asso.'" method="POST">
 				<input type="hidden" name="action" value="nouveau_paiement" />
 				<table>';
 		print "<th>Entité du supplément</th><th>Type du supplément</th><th>Valeur</th><th>Payer à</th>";
@@ -141,7 +146,7 @@ if ($_POST['action'] == 'select_assos' && $self && !empty($_POST['cre']) ) {
 		foreach($tab['assos'] as $row){
 			if (($self || $row['id_asso_paie']==$current_asso)) {
 				print "<tr><td>Association {$assos[$row['id_asso_paie']]['nom']}</td><td>{$row['type']}</td><td>{$row['valeur']}</td><td>{$assos[$row['id_asso_paie']]['nom']}</td>";
-				if ($resp_asso) print "<td><INPUT name=\"sup[{$row['id']}]\" type=\"text\" /></td>";
+				if ($resp_asso) print "<td><INPUT name=\"sup[{$row['id']}]\" value=\"{$_POST['sup'][$row['id']]}\" type=\"text\" /></td>";
 				print "</tr>";
 			}
 
@@ -149,30 +154,31 @@ if ($_POST['action'] == 'select_assos' && $self && !empty($_POST['cre']) ) {
 		foreach($tab['secs'] as $row){
 			if (($self || $row['id_asso_paie']==$current_asso)) {
 				print "<tr><td>Section {$row['nom_sec']}</td><td>{$row['type']}</td><td>{$row['valeur']}</td><td>{$assos[$row['id_asso_paie']]['nom']}</td>";
-				if ($resp_asso) print "<td><INPUT name=\"sup[{$row['id']}]\" type=\"text\" /></td>";
+				if ($resp_asso) print "<td><INPUT name=\"sup[{$row['id']}]\" value=\"{$_POST['sup'][$row['id']]}\" type=\"text\" /></td>";
 				print "</tr>";
 			}
 		}
 		foreach($tab['acts'] as $row){
 			if (($self || $row['id_asso_paie']==$current_asso)) {
 				print "<tr><td>Activité {$row['nom_act']}</td><td>{$row['type']}</td><td>{$row['valeur']}</td><td>{$assos[$row['id_asso_paie']]['nom']}</td>";
-				if ($resp_asso) print "<td><INPUT name=\"sup[{$row['id']}]\" type=\"text\" /></td>";
+				if ($resp_asso) print "<td><INPUT name=\"sup[{$row['id']}]\" value=\"{$_POST['sup'][$row['id']]}\" type=\"text\" /></td>";
 				print "</tr>";
 			}
 		}
 		foreach($tab['cres'] as $row){
 			if (($self || $row['id_asso_paie']==$current_asso)) {
 				print "<tr><td>Créneau {$row['nom_act']} - {$row['jour_cre']} - {$row['debut_cre']}</td><td>{$row['type']}</td><td>{$row['valeur']}</td><td>{$assos[$row['id_asso_paie']]['nom']}</td>";
-				if ($resp_asso) print "<td><INPUT name=\"sup[{$row['id']}]\" type=\"text\" /></td>";
+				if ($resp_asso) print "<td><INPUT name=\"sup[{$row['id']}]\" value=\"{$_POST['sup'][$row['id']]}\" type=\"text\" /></td>";
 				print "</tr>";
 			}
 		}
 		if ($resp_asso){
-			print "<tr><td></td><td></td><td></td><td>Type :</td><td><INPUT name=\"type\" type=\"text\" /></td></tr>";
-			print "<tr><td></td><td></td><td></td><td>Numéro :</td><td><INPUT name\"num\" type=\"text\" /></td></tr>";
-			print "<tr><td></td><td></td><td></td><td>Remarque :</td><td><INPUT name=\"remarque\" type=\"text\" /></td></tr>";
-			print "<tr><td></td><td></td><td></td><td>Promo :</td><td><INPUT name=\"promo\" type=\"text\" /></td></tr>";
+			print "<tr><td></td><td></td><td></td><td>Type :</td><td><INPUT name=\"type\" value=\"{$_POST['type']}\"  type=\"text\" /></td></tr>";
+			print "<tr><td></td><td></td><td></td><td>Numéro :</td><td><INPUT name=\"num\" value=\"{$_POST['num']}\" type=\"text\" /></td></tr>";
+			print "<tr><td></td><td></td><td></td><td>Remarque :</td><td><INPUT name=\"remarque\" value=\"{$_POST['remarque']}\" type=\"text\" /></td></tr>";
+			print "<tr><td></td><td></td><td></td><td>Promo :</td><td><INPUT name=\"promo\" value=\"{$_POST['promo']}\" type=\"text\" /></td></tr>";
 			print "<tr><td></td><td></td><td></td><td>Envoyer :</td><td><INPUT type=\"submit\" /></td></tr>";
+			print "<INPUT type=\"hidden\" name=\"id_adh\" value=$id_adh >";
 		}
 
 		print '</table></FORM>';
