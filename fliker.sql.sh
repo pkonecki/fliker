56b88c69f11f2e80e031e7ec161cd209fd127a60
pb.marty@gmail.com <pb.marty@gmail.com>
2011-08-31 16:47:09 +0000

    bash script to prefix sql tables

---
+++ fliker.sql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SQLFILE=fliker.sql
+OUTFILE=fliker.out
+TMPFILE=fliker.tmp
+
+PREFIX=fliker_
+
+cp -arf $SQLFILE $OUTFILE
+for table in `egrep -i '^(create table|create table if not exists|alter table|insert into) \`' $OUTFILE | grep -v '^\-\- ' | cut -d'\`' -f2 | sort -u` ; do sed "s/\`$table\`/\`$PREFIX$table\`/g" $OUTFILE > $TMPFILE ; mv -f $TMPFILE $OUTFILE ; done
+rm -f  $TMPFILE
