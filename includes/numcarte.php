71b0b1e340f94ce13076f73a8197a2edb463747a
meneo.tk <meneo.tk@a3323499-10a8-7d40-5257-ad8efff166ca>
2011-09-09 13:00:43 +0000

    correction bug numcarte

--- includes/numcarte.php
+++ includes/numcarte.php
@@ -1,7 +1,12 @@
 <?php 
 include("paths.php");
+if(empty($_GET['numcarte'])){
+	echo "true";
+	die();
+}
 include("opendb.php");
-if(mysql_num_rows(mysql_query("SELECT {$GLOBALS['prefix_db']}numcarte FROM adherent WHERE numcarte='".mysql_real_escape_string($_GET['numcarte'])."'"))){
+$res = mysql_query("SELECT numcarte FROM {$GLOBALS['prefix_db']}adherent WHERE numcarte='".mysql_real_escape_string($_GET['numcarte'])."'");
+if(mysql_num_rows($res)){
 	echo "false";
 
 } else {
